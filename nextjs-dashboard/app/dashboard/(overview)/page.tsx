b1a17f7810162aeba007f869e9e367c7b3fd0ba5
jwyang <yjw.raymond@gmail.com>
2025-02-01 16:44:29 +0900

    Modify: Ch9: Streaming a component

--- nextjs-dashboard/app/dashboard/(overview)/page.tsx
+++ nextjs-dashboard/app/dashboard/(overview)/page.tsx
@@ -4,12 +4,22 @@ import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import {
      fetchLatestInvoices,
-     fetchRevenue,
      fetchCardData
     } from '@/app/lib/data';
+/*    
+import {
+    fetchLatestInvoices,
+    fetchRevenue,
+    fetchCardData
+    } from '@/app/lib/data';
+*/
+import { Suspense } from 'react';
+import { RevenueChartSkeleton } from '@/app/ui/skeletons';
+import { revenue } from '@/app/lib/placeholder-data';
 
+       
 export default async function Page() {
-    const revenue = await fetchRevenue();
+    // const revenue = await fetchRevenue();
     const latestInvoices = await fetchLatestInvoices();
     const {
         numberOfInvoices,
@@ -34,8 +44,11 @@ export default async function Page() {
                  /> }
             </div>
             <div className='mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8'>
-                { <RevenueChart revenue={revenue} /> }
-                { <LatestInvoices latestInvoices={latestInvoices} /> }
+                { /* <RevenueChart revenue={revenue} /> */ }
+                <Suspense fallback={<RevenueChartSkeleton />}>
+                    <RevenueChart />
+                 </Suspense>
+                <LatestInvoices latestInvoices={latestInvoices} />
             </div>
         </main>
     );
