1294d230546c73733a4ee889ef55c850f30eefb2
jwyang <yjw.raymond@gmail.com>
2025-02-03 18:07:53 +0900

    Modify: Ch9: Grouping components 2

--- nextjs-dashboard/app/dashboard/(overview)/page.tsx
+++ nextjs-dashboard/app/dashboard/(overview)/page.tsx
@@ -1,3 +1,4 @@
+import CardWrapper from '@/app/ui/dashboard/cards';
 import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
@@ -12,23 +13,32 @@ import {
 */
 import { Suspense } from 'react';
 /* import { RevenueChartSkeleton } from '@/app/ui/skeletons'; */
+/*
 import {
      RevenueChartSkeleton,
      LatestInvoicesSkeleton,
     } from '@/app/ui/skeletons';
-
+*/
+import {
+     RevenueChartSkeleton,
+     LatestInvoicesSkeleton,
+     CardSkeleton,
+    } from '@/app/ui/skeletons';
+   
 /* import { revenue } from '@/app/lib/placeholder-data'; */
 
        
 export default async function Page() {
     // const revenue = await fetchRevenue();
     //const latestInvoices = await fetchLatestInvoices();
+    /*
     const {
         numberOfInvoices,
         numberOfCustomers,
         totalPaidInvoices,
         totalPendingInvoices,
     } = await fetchCardData();
+     */
 
     return (
         <main>
@@ -36,14 +46,18 @@ export default async function Page() {
                 Dashboard
             </h1>
             <div className='grid gap-6 sm:grid-cols-2 lg:grid-cols-4'>
-                { <Card title="Collected" value={totalPaidInvoices} type="collected" /> }
-                { <Card title="Pending" value={totalPendingInvoices} type="pending" /> }
-                { <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />}
-                { <Card
+                { /* <Card title="Collected" value={totalPaidInvoices} type="collected" /> */ }
+                { /* <Card title="Pending" value={totalPendingInvoices} type="pending" /> */ }
+                { /* <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> */ }
+                { /* <Card
                     title="Total Customers"
                     value={numberOfCustomers}
                     type="customers"
-                 /> }
+                 /> */ }
+                < Suspense fallback={<CardSkeleton />} >
+                    <CardWrapper />
+                </Suspense>
+
             </div>
             <div className='mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8'>
                 { /* <RevenueChart revenue={revenue} /> */ }
