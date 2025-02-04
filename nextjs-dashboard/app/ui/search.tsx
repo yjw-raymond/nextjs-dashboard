11c2a1f07b566e25ca4822e755263d36cda80d1e
jwyang <yjw.raymond@gmail.com>
2025-02-04 13:55:21 +0900

    Ch11: Best practice: Debouncing

--- nextjs-dashboard/app/ui/search.tsx
+++ nextjs-dashboard/app/ui/search.tsx
@@ -2,22 +2,40 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
+  
   function handleSearch(term: string) {
+    console.log(`Searching... ${term}`);
+
     const params = new URLSearchParams(searchParams);
     if (term) {
       params.set('query', term);
     } else {
       params.delete('query');
     }
-    console.log(term);
     replace(`${pathname}?${params.toString()}`);
   }
+  
+
+  /*
+  const handleSearch = useDebouncedCallback((term) => {
+    console.log(`Searching... ${term}`);
+
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 1000);
+  */
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
