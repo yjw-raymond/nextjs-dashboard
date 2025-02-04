27649b2f65d11e6a5499c1b4681d0fe00a322975
jwyang <yjw.raymond@gmail.com>
2025-02-04 14:46:01 +0900

    Ch11: Adding pagination

--- nextjs-dashboard/app/ui/search.tsx
+++ nextjs-dashboard/app/ui/search.tsx
@@ -9,7 +9,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  
+  /*
   function handleSearch(term: string) {
     console.log(`Searching... ${term}`);
 
@@ -21,13 +21,14 @@ export default function Search({ placeholder }: { placeholder: string }) {
     }
     replace(`${pathname}?${params.toString()}`);
   }
-  
+  */
 
-  /*
+  
   const handleSearch = useDebouncedCallback((term) => {
     console.log(`Searching... ${term}`);
 
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
@@ -35,7 +36,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     }
     replace(`${pathname}?${params.toString()}`);
   }, 1000);
-  */
+  
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
