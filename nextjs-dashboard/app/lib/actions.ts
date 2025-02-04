e6f91c511d88bc012e8d8c19effffb8611d9847c
jwyang <yjw.raymond@gmail.com>
2025-02-04 17:15:08 +0900

    Ch12.6: Revalidate and redirect

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -2,6 +2,8 @@
 
 import { z } from 'zod';
 import postgres from 'postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 
 const FormSchema = z.object({
     id: z.string(),
@@ -45,4 +47,7 @@ export async function createInvoice(formData: FormData) {
         INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
 }
