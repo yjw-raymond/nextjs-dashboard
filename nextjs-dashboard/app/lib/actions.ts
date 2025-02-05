f3769908c4231aed2c5ed46b847605749fc87357
jwyang <yjw.raymond@gmail.com>
2025-02-05 15:42:10 +0900

    Ch14: Server-Side validation

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -5,12 +5,26 @@ import postgres from 'postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
+export type State = {
+    errors?: {
+        customerId?: string[];
+        amount?: string[];
+        status?: string[];
+    };
+    message?: string | null;
+};
 
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
+    customerId: z.string({
+        invalid_type_error: 'Please select a customer.',
+    }),
+    amount: z.coerce
+        .number()
+        .gt(0, { message: 'Please enter an amount greater than $0.' }),
+    status: z.enum(['pending', 'paid'], {
+        invalid_type_error: 'Please select an invoice status.',
+    }),
     date: z.string(),
 });
 
@@ -20,7 +34,8 @@ const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
+/* export async function createInvoice(formData: FormData) { */
+export async function createInvoice(prevState: State, formData: FormData) {
     /*
     const rawFormData = {
         customerId: formData.get('customerId'),
@@ -31,11 +46,31 @@ export async function createInvoice(formData: FormData) {
     */
     /* console.log(typeof rawFormData.amount); */
 
+    /*
     const { customerId, amount, status } = CreateInvoice.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
+    */
+    const validateFields = CreateInvoice.safeParse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+
+    // If form validation fails, return errors early, Ohterwise, continue
+    if (!validateFields.success) {
+        //console.log('Error createInvoice...');
+        return {
+          errors: validateFields.error.flatten().fieldErrors,
+          message: 'Missing Fields. Failed to Create Invoice.',  
+        };
+    }
+
+    // Prepare data for insertion into the database
+    const { customerId, amount, status } = validateFields.data;
+
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
@@ -52,7 +87,12 @@ export async function createInvoice(formData: FormData) {
             VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
         `;
     } catch (error) {
-        console.error(error);
+        // If a database error occurs, return a more specific error.
+        // console.error(error);
+        return {
+            message: 'Database Error: Failed to Create Invoice.',
+        };
+
     }
 
     revalidatePath('/dashboard/invoices');
