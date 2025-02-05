1f66fe29cf154338cfe87dca82eb4bf0d420e933
jwyang <yjw.raymond@gmail.com>
2025-02-05 10:33:51 +0900

    Ch13: TEST: Error function in NextJS

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -79,6 +79,8 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
+    // throw new Error('Failed to Delete Invoice');
+
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
 }
